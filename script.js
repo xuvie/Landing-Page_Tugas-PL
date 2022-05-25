825d99630d4b795e1ed5c318722ad2ed6ff13f41
xuvie <rifqifahrezi310@gmail.com>
2022-05-25 13:13:08 +0700

    add animation

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".button");
+button.addEventListener("click", (e) => {
+	e.preventDefault();
+	button.classList.add("button--clicked");
+	document.querySelectorAll("span").forEach((element) => {
+		element.classList.add("expanded");
+	});
+
+	setTimeout(() => {
+		button.classList.remove("button--clicked");
+	}, 3500);
+	setTimeout(() => {
+		document.querySelectorAll("span").forEach((element) => {
+			element.classList.remove("expanded");
+		});
+	}, 1700);
+});
